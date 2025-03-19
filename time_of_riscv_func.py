9a0eb695792e59fa0b4f26c7050ad6dfbc079f65
Charles Averill <charlesaverill20@gmail.com>
2025-03-19 15:18:10 -0500

    Invert addr offset

--- time_of_riscv_func.py
+++ time_of_riscv_func.py
@@ -60,22 +60,22 @@ def print_bb(results, function_name=""):
         blocks = func_data["blocks"]
         if not len(blocks):
             continue
-        block_addr = blocks[0]['bb_start_vaddr'] - GHIDRA_ADDR_OFFSET
+        block_addr = blocks[0]['bb_start_vaddr'] + GHIDRA_ADDR_OFFSET
         print(f"{block_addr:010x}  <{func_name}>:")
 
         for block in blocks:
-            preblock = "    > Src: " + ", ".join(f"{addr-GHIDRA_ADDR_OFFSET:x}" for addr in block["source_vaddrs"])
+            preblock = "    > Src: " + ", ".join(f"{addr+GHIDRA_ADDR_OFFSET:x}" for addr in block["source_vaddrs"])
             if block["is_entry_point"]:
                 preblock += "(entry)"
             print(preblock)
-            block_addr = block["bb_start_vaddr"] - GHIDRA_ADDR_OFFSET
+            block_addr = block["bb_start_vaddr"] + GHIDRA_ADDR_OFFSET
             for instr in block["instructions"]:
                 byte_instr = instr["instruction_byte"]
                 str_instr = instr["instruction_str"]
-                addr = instr["instr_offset"] - GHIDRA_ADDR_OFFSET
+                addr = instr["instr_offset"] + GHIDRA_ADDR_OFFSET
                 print(f"{addr:7x}:   {byte_instr:20s} {str_instr}") # - {time_of_riscv_instr(instr['mnem'], instr['operands'], 's', 'ML')}")
 
-            problock = "    > Dst: " + ", ".join(f"{addr-GHIDRA_ADDR_OFFSET:x}" for addr in block["exit_vaddrs"])
+            problock = "    > Dst: " + ", ".join(f"{addr+GHIDRA_ADDR_OFFSET:x}" for addr in block["exit_vaddrs"])
             if block["is_exit_point"]:
                 problock += "(exit)"
             print(problock)
@@ -95,7 +95,7 @@ def time_of_function_basic_blocks(results, function_name):
 	out += "  let err_time := 999 in\n"
 	out += "  match addr with\n"
 	for block in func["blocks"]:
-		out += f"  | {block['instructions'][-1]['instr_offset'] - GHIDRA_ADDR_OFFSET} => {time_of_basic_block(block)}\n"
+		out += f"  | {hex(block['instructions'][-1]['instr_offset'] + GHIDRA_ADDR_OFFSET)} => {time_of_basic_block(block)}\n"
 	out += "  end.\n\n"
 	return out
 
@@ -116,7 +116,10 @@ if __name__ ==  "__main__":
 	parser = argparse.ArgumentParser('time_of_riscv_function.py', description='pretty print parsed json file')
 	parser.add_argument("bin")
 	parser.add_argument("--function_name", default="")
+	parser.add_argument("--addr_offset", default=0, type=int)
 	args = parser.parse_args()
+
+	GHIDRA_ADDR_OFFSET = args.addr_offset
 
 	# If preproc file doesn't exist, generate
 	preproc_fn = f"{args.bin}.preprocessed.json"
